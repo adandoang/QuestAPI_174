06198cad0d78bc0025d024b36f10919e477a1038
adandoang <akhdanjzif@gmail.com>
2024-12-29 22:54:39 +0700

    membuat object DestinasiHome

---
+++ app/src/main/java/com/example/week12/view/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.week12.view
+
+import com.example.week12.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
