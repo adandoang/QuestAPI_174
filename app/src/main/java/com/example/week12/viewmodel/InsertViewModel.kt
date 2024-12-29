0768610dd96b50bca8065f47ec8052fd60a7a772
adandoang <akhdanjzif@gmail.com>
2024-12-29 21:45:27 +0700

    membuat data class InsertUiState dan InsertUiEvent

--- app/src/main/java/com/example/week12/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/week12/viewmodel/InsertViewModel.kt
@@ -14,3 +14,16 @@ class InsertViewModel (private val mhs: MahasiswaRepository: ViewModel) {
         uiState =InsertUiState(insertUiEvent = insertUiEvent)
     }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class  InsertUiEvent(
+    val nim: String="",
+    val nama: String="",
+    val alamat: String="",
+    val jenisKelamin: String="",
+    val kelas: String="",
+    val angkatan: String=""
+)
