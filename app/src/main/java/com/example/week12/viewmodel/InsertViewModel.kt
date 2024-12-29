e1077713adcc9bd7109f515762c2e3b95d9f86e8
adandoang <akhdanjzif@gmail.com>
2024-12-29 21:57:11 +0700

    menyelesaikan class InsertViewModel bagian suspend fun inserMhs

--- app/src/main/java/com/example/week12/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/week12/viewmodel/InsertViewModel.kt
@@ -4,15 +4,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.week12.model.Mahasiswa
 import com.example.week12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
-class InsertViewModel (private val mhs: MahasiswaRepository: ViewModel) {
+
+class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
         private set
 
-    fun updateInsertMhsState(insertUiEvent InsertUiEvent) {
-        uiState =InsertUiState(insertUiEvent = insertUiEvent)
+    fun updateInsertMhsState(insertUiEvent:InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch{
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
     }
 }
 
