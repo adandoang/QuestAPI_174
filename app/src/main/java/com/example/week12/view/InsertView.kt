744b55719c09847694d37c174e39eefc5396e11e
adandoang <akhdanjzif@gmail.com>
2024-12-29 22:16:14 +0700

    membuat object DestinasiEntry

---
+++ app/src/main/java/com/example/week12/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.week12.view
+
+import com.example.week12.navigation.DestinasiNavigasi
+
+object DestinasiEntry:DestinasiNavigasi{
+    override val route ="item_entry"
+    override val titleRes = "Entry Mhs"
+}
