1df60e9e1b019b7a4184a626b95b9e4a1e9547fe
adandoang <akhdanjzif@gmail.com>
2024-12-29 22:29:21 +0700

    membuat InsertView.kt, fun EntryMhsScreen

--- app/src/main/java/com/example/week12/view/InsertView.kt
+++ app/src/main/java/com/example/week12/view/InsertView.kt
@@ -1,8 +1,47 @@
 package com.example.week12.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.traceEventEnd
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.week12.customwidget.CostumeTopAppBar
 import com.example.week12.navigation.DestinasiNavigasi
+import com.example.week12.viewmodel.InsertViewModel
+import java.security.KeyStore.Entry
 
 object DestinasiEntry:DestinasiNavigasi{
     override val route ="item_entry"
     override val titleRes = "Entry Mhs"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: ()-> Unit,
+    modifier: Modifier=Modifier,
+    viewModel: InsertViewModel= viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier=modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+
+    }
+}
+
